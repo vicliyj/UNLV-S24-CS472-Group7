be2c5c4c4f6e282eb08065e295e1334c1bcda07e
MoldyPotat <106991015+MoldyPotat@users.noreply.github.com>
2024-02-26 13:36:25 -0800

    Update AlexanderSchupp_InefficentCode.cpp
    Fixes Issue #41 now runs much more efficiently and much easier to understand.

--- random_code/AlexanderSchupp_InefficentCode.cpp
+++ random_code/AlexanderSchupp_InefficentCode.cpp
@@ -1,30 +1,16 @@
-void sortBi(BigramDictionary& dict)
-{
-	//bubble sort
-	for (int i = 0; i < dict.numberOfElements - 1; i++)
-	{
-		for (int j = 0; j < dict.numberOfElements - i - 1; j++)
-		{
-			//sort by words
-			if (dict.bigrams[j].word2 > dict.bigrams[j + 1].word2)
-			{
-				//swaps 
-				Bigram temp = dict.bigrams[j];
-				dict.bigrams[j] = dict.bigrams[j + 1];
-				dict.bigrams[j + 1] = temp;
-			}
-		}
+#include <algorithm>
 
-		for (int j = 0; j < dict.numberOfElements - i - 1; j++)
-		{
-			//sort of frequency
-			if (dict.bigrams[j].word1 > dict.bigrams[j + 1].word1)
-			{
-				//swaps 
-				Bigram temp = dict.bigrams[j];
-				dict.bigrams[j] = dict.bigrams[j + 1];
-				dict.bigrams[j + 1] = temp;
-			}
-		}
-	}
+// Define a custom comparator function to sort bigrams alphabetically
+bool compareBigrams(const Bigram& bg1, const Bigram& bg2) {
+    // First, compare word1
+    if (bg1.word1 != bg2.word1) {
+        return bg1.word1 < bg2.word1;
+    }
+    // If word1 is the same, compare word2
+    return bg1.word2 < bg2.word2;
+}
+
+void sortBi(BigramDictionary& dict) {
+    // Use std::sort with the custom comparator
+    std::sort(dict.bigrams, dict.bigrams + dict.numberOfElements, compareBigrams);
 }
